8d04fd379a06e8721e0653113858dc6b598bb49a
Air <69145482+AiryAir@users.noreply.github.com>
2023-03-25 11:57:00 +0530

    Create turtlesim_pose.py

---
+++ turtlesim_pose.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+import rospy
+from std_msgs.msg import Pose, Twist
+
+#task 1. import the Pose type from the module turtlesim
+
+def move():
+    twist = Twist()
+    pub = rospy.Publisher('/turtle1/cmd_vel', twist, queue_size=10)
+
+    twist.linear.x = 1.0
+    pub.publish()
+
+def poseCallback(pose_message):
+
+   #task 4. display the x, y, and theta received from the message
+    print ("pose callback")
+    print ('x = ' % Pose.x)
+    print ('y = %f' % Pose.y)
+    print ('yaw = '% Pose.theta) 
+
+if __name__ == '__main__':
+    try:
+        
+        rospy.init_node('turtlesim_motion_pose', anonymous=True)        
+
+       #task 2. subscribe to the topic of the pose of the Turtlesim
+        rospy.Subscriber("/turtlesim/Pose", Pose, queue_size=10)
+
+       #task 3. spin
+        rospy.spin()
+
+       
+    except rospy.ROSInterruptException:
+        rospy.loginfo("node terminated.")
